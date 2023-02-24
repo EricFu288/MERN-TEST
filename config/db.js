61469d20c964c0c188a2176d8118a27a808c015f
Eric Fu <124872398+EricFu288@users.noreply.github.com>
2023-02-24 11:51:27 -0800

    update db url

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URL, {
+    const conn = await mongoose.connect("mongodb://galaxy:orKxG14IZsrpPxbtN6MOEB5AJ9bXVRuRkcm9Mp6CXsucYAFrYEGe7Swoahh1vEaNHQs7nQ7h02Z5ACDbAAInew==@galaxy.mongo.cosmos.azure.com:10255/test?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@galaxy@", {
       useUnifiedTopology: true,
       useNewUrlParser: true,
     });
